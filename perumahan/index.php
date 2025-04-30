aa6dbac3f565ad6475fd47a3e3906c9d011ca190
Irfan mba <ip.5090@gmail.com>
2025-04-30 10:35:02 +0800

    template perumahan

---
+++ perumahan/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Perumahan</title>
+</head>
+<body>
+    <div class="container">
+        <h3 class="alert alert-success">Data Perumahan</h3>
+
+        <i>Letakkan tabel disini.</i>
+    </div>
+</body>
+</html>
